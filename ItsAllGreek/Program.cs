4b1399ca1e207bdff870c48c4e9b013a79ddda75
puglisac <alan.c.puglisi@gmail.com>
2020-11-24 11:17:26 -0600

    problem solved

---
+++ ItsAllGreek/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ItsAllGreek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter starting X value");
+            float point1x = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter starting Y value");
+            float point1y = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter ending X value");
+            float point2x = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter ending Y value");
+            float point2y = float.Parse(Console.ReadLine());
+            float deltaX = delta(point2x, point1x);
+            float deltaY = delta(point2y, point1y);
+            Console.WriteLine($"You moved {distance(deltaX,deltaY)} units along {getAngle(deltaX, deltaY)} degrees");
+
+        }
+
+        static float delta(float x, float y)
+        {
+            return x - y;
+        }
+
+        static float distance(float deltaX, float deltaY)
+        {
+            return MathF.Sqrt((deltaX*deltaX) + (deltaY*deltaY));
+        }
+
+        static float getAngle(float x, float y)
+        {
+            float radians = MathF.Atan2(x, y);
+            return radians * (180 / MathF.PI);
+        }
+    }
+}
